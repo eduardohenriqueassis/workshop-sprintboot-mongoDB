dec0f0be253e78d03f45d181d8437b0b3e33e12e
Eduardo Assis <eduardohenriqueassis1973@gmail.com>
2024-05-23 19:04:00 -0300

    getting userś posts

--- src/main/java/com/eduardoassis/workshopmongo/resources/UserResource.java
+++ src/main/java/com/eduardoassis/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.eduardoassis.workshopmongo.domain.Post;
 import com.eduardoassis.workshopmongo.domain.User;
 import com.eduardoassis.workshopmongo.dto.UserDTO;
 import com.eduardoassis.workshopmongo.services.UserService;
@@ -64,6 +65,12 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 		
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 		
 }
