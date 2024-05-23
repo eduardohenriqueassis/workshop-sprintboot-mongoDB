7f899d6d812b75519a6153a3a6d7d802cb70c677
Eduardo Assis <eduardohenriqueassis1973@gmail.com>
2024-05-23 19:14:51 -0300

    get post by id

---
+++ src/main/java/com/eduardoassis/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.eduardoassis.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardoassis.workshopmongo.domain.Post;
+import com.eduardoassis.workshopmongo.repository.PostRepository;
+import com.eduardoassis.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+	
+}
